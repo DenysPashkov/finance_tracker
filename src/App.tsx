0a18c50eaacdc5e4a63dea3655855efdc1a5a67c
DenysPashkov <denyspashkov@yahoo.com>
2025-05-14 12:40:21 +0200

    created the base structure of the view

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          backgroundColor: "red",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "20px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <p>Header</p>
+          <p>light-dark-Mode</p>
+        </header>
+        <div style={{ flex: 1, backgroundColor: "gray" }}>
+          <div style={{ backgroundColor: "green", padding: "10px" }}>
+            <p style={{ margin: 0 }}>navigation bar</p>
+          </div>
+          <p>content</p>
+        </div>
+        <footer style={{ backgroundColor: "lightblue", padding: "20px" }}>
+          Footer
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
