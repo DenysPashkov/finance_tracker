43bceb6eb5e18027c38c127217bab3c82da3717f
DenysPashkov <denyspashkov@yahoo.com>
2025-05-15 12:09:51 +0200

    implemented financeCell, patrimonyCell and historyCell graphically with the relative component needed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const contentCSS: CSSProperties = {
     flex: 1,
-    backgroundColor: "gray",
+    backgroundColor: "lightgray",
     padding: "30px",
     display: "flex",
     flexDirection: "column",
@@ -22,7 +22,6 @@ export default function Home() {
   const bodyCSS: CSSProperties = {
     height: "100%",
     width: "100%",
-    backgroundColor: "red",
     display: "flex",
     flexDirection: "column",
   };
@@ -40,13 +39,21 @@ export default function Home() {
     return (
       <div
         style={{
-          flex: 2,
+          flex: 1,
           ...cellCSS,
         }}
       >
         <h2>Finanze</h2>
-        <p>Money</p>
-        <p>dots</p>
+        <h1
+          style={{
+            textAlign: "center",
+            color:
+              "green" /* TODO: Manage the possibility to have a negative finance and change the color of the Text here */,
+          }}
+        >
+          20 $
+        </h1>
+        {/*TODO: Add dots in the future once we will have multiple finances <p>dots</p> */}
         <div
           style={{
             display: "flex",
@@ -54,22 +61,72 @@ export default function Home() {
             justifyContent: "space-evenly",
           }}
         >
-          <p>add</p>
-          <p>rem</p>
-          <p>send?</p>
+          <button>Aggiungi</button>
+          <button>Preleva</button>
+          <button>Trasferisci</button>
+          <button>Investi</button>
         </div>
+      </div>
+    );
+  }
+
+  function ShowMoreRow() {
+    return (
+      <div
+        style={{
+          backgroundColor: "lightgray",
+          padding: "10px",
+        }}
+      >
+        <h4 style={{ margin: "0px", textAlign: "center" }}>Mostra di piu'</h4>
+      </div>
+    );
+  }
+
+  function CellContent({ children }: { children: React.ReactNode }) {
+    return (
+      <div
+        style={{
+          borderRadius: "8px",
+          overflow: "hidden",
+          backgroundColor: "rgba(0,0,0,0.05)",
+        }}
+      >
+        {children}
       </div>
     );
   }
 
   function PatrimonyCell() {
     return (
-      <div style={{ flex: 1, ...cellCSS }}>
+      <div style={{ flex: 2, ...cellCSS }}>
         <h2>Patrimonio</h2>
-        <p>riga 1</p>
-        <p>riga 2</p>
-        <p>riga 3</p>
-        <p>more</p>
+        <CellContent>
+          <PatrimonyRow title="Liquidi" value={20} />
+          <PatrimonyRow title="Investimenti" value={-20} />
+          <PatrimonyRow title="Criptovalute" value={20} />
+          <ShowMoreRow />
+        </CellContent>
+      </div>
+    );
+  }
+
+  function PatrimonyRow({ title, value }: { title: string; value: number }) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderBlockEnd: "1px solid gray",
+          paddingInline: "30px",
+          backgroundColor:
+            value >= 0 ? "rgba(0, 255, 17, 0.35)" : "rgba(255, 0, 0, 0.35)",
+        }}
+      >
+        <h4>{title}</h4>{" "}
+        <h5 style={{ color: value >= 0 ? "black" : "black" }}>{value}$</h5>
       </div>
     );
   }
@@ -77,11 +134,66 @@ export default function Home() {
   function HistoryCell() {
     return (
       <div style={cellCSS}>
-        <h2>Storio</h2>
-        <p>spesa 1</p>
-        <p>spesa 2</p>
-        <p>spesa 3</p>
-        <p>more</p>
+        <h2>Storico</h2>
+        <CellContent>
+          <HistoryRow company="Amazon" date="15/05/2025" cost={24.99} />
+          <HistoryRow company="Bar" date="15/05/2025" cost={3.5} />
+          <HistoryRow company="Amazon" date="15/05/2025" cost={24.99} />
+          <ShowMoreRow />
+        </CellContent>
+      </div>
+    );
+  }
+
+  function HistoryRow({
+    company,
+    date,
+    cost,
+  }: {
+    company: string;
+    date: string;
+    cost: number;
+  }) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "start",
+          paddingInline: "15px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "gray",
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+            }}
+          />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h3 style={{ marginBlockEnd: "0px" }}>{company}</h3>
+            <p
+              style={{
+                marginBlockStart: "0px",
+                color: "grayText",
+                fontSize: "8px",
+              }}
+            >
+              {date}
+            </p>
+          </div>
+        </div>
+        <h3>{cost.toFixed(2)} $</h3>
       </div>
     );
   }
