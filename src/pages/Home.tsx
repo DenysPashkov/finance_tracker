fa1b2b247d33443f29575cb49289570550f82f62
DenysPashkov <denyspashkov@yahoo.com>
2025-05-15 10:53:33 +0200

    refactored the code adding helping function inside home for improve the readability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import type { CSSProperties } from "react";
 
-function Home() {
+export default function Home() {
   const cellCSS: CSSProperties = {
     backgroundColor: "white",
     borderRadius: "8px",
@@ -16,6 +16,7 @@ function Home() {
     display: "flex",
     flexDirection: "column",
     gap: "20px",
+    minWidth: "700px",
   };
 
   const bodyCSS: CSSProperties = {
@@ -26,66 +27,89 @@ function Home() {
     flexDirection: "column",
   };
 
+  function FirstRow() {
+    return (
+      <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+        <div
+          style={{
+            flex: 2,
+            ...cellCSS,
+          }}
+        >
+          <h2>Finanze</h2>
+          <p>Money</p>
+          <p>dots</p>
+          <div
+            style={{
+              display: "flex",
+              gap: "10px",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <p>add</p>
+            <p>rem</p>
+            <p>send?</p>
+          </div>
+        </div>
+        <div
+          style={{
+            flex: 1,
+            ...cellCSS,
+          }}
+        >
+          <h2>Spese mensili</h2>
+          <p>graph</p>
+          <p>money used</p>
+        </div>
+      </div>
+    );
+  }
+
+  function SecondRow() {
+    return (
+      <div style={cellCSS}>
+        <h2>Storio</h2>
+        <p>spesa 1</p>
+        <p>spesa 2</p>
+        <p>spesa 3</p>
+        <p>more</p>
+      </div>
+    );
+  }
+
+  function ThirdRow() {
+    return (
+      <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+        <div style={{ flex: 1, ...cellCSS }}>
+          <h2>pagamenti programmati</h2>
+          <p>riga 1</p>
+          <p>riga 2</p>
+          <p>riga 3</p>
+          <p>more</p>
+        </div>
+
+        <div style={{ flex: 1, ...cellCSS }}>
+          <h2>Patrimonio</h2>
+          <p>riga 1</p>
+          <p>riga 2</p>
+          <p>riga 3</p>
+          <p>more</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <div style={bodyCSS}>
         <Header />
         <div style={contentCSS}>
-          <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <div
-              style={{
-                flex: 2,
-                ...cellCSS,
-              }}
-            >
-              <p>Money</p>
-              <p>dots</p>
-              <div style={{ display: "flex", gap: "10px" }}>
-                <p>add</p>
-                <p>rem</p>
-                <p>send?</p>
-              </div>
-            </div>
-            <div
-              style={{
-                flex: 1,
-                ...cellCSS,
-              }}
-            >
-              <p>title</p>
-              <p>graph</p>
-              <p>money used</p>
-            </div>
-          </div>
-          <div style={cellCSS}>
-            <p>title storico</p>
-            <p>spesa 1</p>
-            <p>spesa 2</p>
-            <p>spesa 3</p>
-            <p>more</p>
-          </div>
-          <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <div style={{ flex: 1, ...cellCSS }}>
-              <p>pagamenti programmato titolo</p>
-              <p>riga 1</p>
-              <p>riga 2</p>
-              <p>riga 3</p>
-              <p>more</p>
-            </div>
-
-            <div style={{ flex: 1, ...cellCSS }}>
-              <p>divisione patrimonio titolo</p>
-              <p>riga 1</p>
-              <p>riga 2</p>
-              <p>riga 3</p>
-              <p>more</p>
-            </div>
-          </div>
+          <FirstRow />
+          <SecondRow />
+          <ThirdRow />
         </div>
         <Footer />
       </div>
     </>
   );
 }
-
-export default Home;
